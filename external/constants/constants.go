cf9c409558cda35c0105cbc1c7dd4568fff4a9f5
Fajar <fajar.rizki@webimp.com.sg>
2020-04-02 18:19:47 +0700

    added docker config

---
+++ external/constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	// ENVPORT const
+	ENVPORT = "PORT"
+	// ENVCONFIGFILE const
+	ENVCONFIGFILE = "$GOPATH/src/gitlab.com/auth-service"
+	// ENVCONFIGPATH const
+	ENVCONFIGPATH = "/app"
+)
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gitlab.com/auth-service/external/config"
+	"gitlab.com/auth-service/external/constants"
+)
+
+func main() {
+	v := config.NewConfig(constants.ENVCONFIGFILE)
+	r := gin.New()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(v.GetString(constants.ENVPORT))
+}
