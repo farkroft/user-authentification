93713c27f4771ffa17b15c8286b9843114893a98
Fajar <fajar.rizki@webimp.com.sg>
2020-04-03 12:52:57 +0700

    refactor server and implement clean arch on server

--- external/database/database.go
+++ external/database/database.go
@@ -2,15 +2,41 @@ package database
 
 import (
 	"fmt"
+	"log"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
 
 	"github.com/jinzhu/gorm"
 	"gitlab.com/auth-service/external/config"
 	"gitlab.com/auth-service/external/constants"
 )
 
+// Repository repository
+type Repository interface {
+	NewDatabase(*config.Config) *Database
+}
+
 // Database struct
 type Database struct {
 	db *gorm.DB
+}
+
+// Base model
+type Base struct {
+	ID        uuid.UUID  `json:"id" gorm:"type:uuid;primary_key"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at"`
+}
+
+func (base *Base) beforeCreate(scope *gorm.Scope) error {
+	uuid, err := uuid.NewV4()
+	if err != nil {
+		log.Printf("error when generate uuid %s", err.Error())
+		return err
+	}
+	return scope.SetColumn("ID", uuid)
 }
 
 // NewDatabase init new database
